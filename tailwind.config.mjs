d783622970b2fcc3a23acc6c51f20ef93e12b578
PieroGiovanni <pierogiovanni933@gmail.com>
2023-12-30 04:21:04 -0500

    added dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  // darkMode: "class",
+  darkMode: "class",
   plugins: [],
 };
