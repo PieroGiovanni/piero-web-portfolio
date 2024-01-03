c1a328c36991eb8f175cd6e9fc7cf846884239ca
PieroGiovanni <pierogiovanni933@gmail.com>
2024-01-03 03:51:55 -0500

    picture of tosh and projects updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
+import Compress from "astro-compress";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), Compress()],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
